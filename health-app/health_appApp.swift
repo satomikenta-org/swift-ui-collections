6087b230450529a707c95353b22b79e41f6b9d74
satomi kenta <satomikennta1213@gmail.com>
2021-04-01 12:49:01 +0900

    Initial Commit

---
+++ health-app/health_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_appApp.swift
+//  health-app
+//
+//  Created by 里見健太 on 2021/04/01.
+//
+
+import SwiftUI
+
+@main
+struct health_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
