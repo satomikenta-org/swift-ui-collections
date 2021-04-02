4027fa99e61632aef9d7dc771a1dd9e0dd1645fc
satomi kenta <satomikennta1213@gmail.com>
2021-04-02 12:51:10 +0900

    add: AppLifecycle and ViewLifecycle

---
+++ health-app/ProfileDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileDetailView.swift
+//  health-app
+//
+//  Created by 里見健太 on 2021/04/02.
+//
+
+import SwiftUI
+
+struct ProfileDetailView: View {
+    var body: some View {
+        Text("Your Profile")
+    }
+}
+
+struct ProfileDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileDetailView()
+    }
+}
--- health-app/SettingView.swift
+++ health-app/SettingView.swift
@@ -5,11 +5,46 @@
 //  Created by 里見健太 on 2021/04/01.
 //
 
+
+// FEATURES
+//
+// ・Lifecycle (active, inactive background)
+// ・NavigationLink
+
+// onAppearは, 初回Mount時, 画面遷移先から戻った時にも呼ばれている
+
 import SwiftUI
 
 struct SettingView: View {
+    @Environment(\.scenePhase) private var scenePhase
     var body: some View {
-        Text("Setting")
+        NavigationView {
+            VStack {
+                Button(action: {},
+                label: {
+                    NavigationLink("Go to Detail", destination: ProfileDetailView())
+                })
+                .frame(maxWidth: .infinity)
+                .frame(height: 40, alignment: .center)
+                .background(Color.blue)
+                .cornerRadius(20)
+                .padding()
+            }
+            .onAppear(perform: {
+                print("OnAppear")
+            })
+            .onChange(of: scenePhase) { scene in
+                switch scene {
+                case .active:
+                    print("scenePhase: active")
+                case .inactive:
+                    print("scenePhase: inactive")
+                case .background:
+                    print("scenePhase: background")
+                @unknown default: break
+                }
+            }
+        }
     }
 }
 
